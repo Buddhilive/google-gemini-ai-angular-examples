2aefcc61803f821af6e5e1c60443693911b45067
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-10-26 21:32:34 +0530

    Update README

--- projects/gemini-nano-browser/src/app/app.component.ts
+++ projects/gemini-nano-browser/src/app/app.component.ts
@@ -16,18 +16,18 @@ export class AppComponent {
   }
 
   async testAI() {
-    // Checks if  ai capabilities are supported by browser
+    // Checks if  ai capabilities are supported by the browser
     const chromeAI = await (window as any).ai?.assistant.capabilities();
 
     if (chromeAI) {
-      // Creates session with gemini nano model. Note: options arg optional
+      // Creates session with gemini nano model.
       const session = await (window as any).ai.assistant.create();
 
       // Execute a prompt
       let answer = await session.prompt("What is AI?");
       console.log(answer);
 
-      // destroys the session
+      // Destroys the session
       await session.destroy();
     } else {
       console.log('Chrome Built-In AI not supported.')
